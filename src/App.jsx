aecd1e7fb936ca0652d84073dc197b3f7efd934c
lokeshagg13 <whumiesstore@gmail.com>
2026-06-22 22:58:29 +0530

    [REFACTOR] - Layout code be part of UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 
-import Layout from "./components/layout/Layout";
+import Layout from "./components/ui/layout/Layout";
 import Dashboard from "./pages/Dashboard";
 import FirmReports from "./pages/FirmReports";
 import MarketplaceReports from "./pages/MarketplaceReports";
